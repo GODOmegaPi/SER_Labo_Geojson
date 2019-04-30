5cf29567784c0279e9f889a933687b817b98b68c
GODOmegaPi <guillaume.laubscher@heig-vd.ch>
2019-04-30 08:53:26 +0200

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class Application {
+    String FILE = "data/countries.geojson";
+
+    public static void main(String[] args)
+    {
+
+    }
+}
